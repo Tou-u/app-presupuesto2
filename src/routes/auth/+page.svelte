71a91c7361d381911d2ad3e89533ef61d2d6e1ee
Tou-u <rodrignacio99@hotmail.com>
2023-10-30 16:58:41 -0300

    arreglo interfaz en movil

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -3,16 +3,16 @@
   import Github from '$lib/icons/Github.svelte'
 </script>
 
-<div class="flex h-screen flex-col items-center justify-center">
-  <div class="m-auto">
+<div class="flex h-screen flex-col">
+  <div class="flex flex-1 flex-col items-center justify-center">
     <h1 class="text-2xl">Inicia sesión</h1>
     <button class="variant-ghost-primary btn mt-5" on:click={() => signIn('github')}>
       <span><Github /></span>
       <span><p class="font-bold">GitHub</p></span>
     </button>
   </div>
-  <small>Inicia sesión para probar las características de la app de presupuesto</small>
-  <div>
-    <p class="p-2 text-center">Dev by Tou ❤️</p>
-  </div>
+  <p class="p-2 text-center">
+    <small>Inicia sesión para probar las características de la app de presupuesto</small>
+  </p>
+  <p class="p-2 text-center">Dev by Tou ❤️</p>
 </div>
