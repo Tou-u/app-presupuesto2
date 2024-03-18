cfbf243ee526bd0db5ae717a5558df73c61cb4b9
Tou-u <rodrignacio99@hotmail.com>
2024-03-18 13:03:08 -0300

    cron job

--- src/functions/health.ts
+++ src/functions/health.ts
@@ -1,6 +1,6 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
 export async function handler() {
-  await fetch('/api/health')
+  await fetch('https://d3224yb3f2wh9l.cloudfront.net/api/health')
   console.log('--- cron job ---')
 }
